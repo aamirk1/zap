fdc7e8e0a2909878e32aa4a8399cc0e2dc24cdbe
aamirk1 <aamir.k7199@gmail.com>
2023-01-05 15:40:05 +0530

    up

--- instapeels/store/urls.py
+++ instapeels/store/urls.py
@@ -8,19 +8,23 @@ urlpatterns = [
     path('collection/<str:cate_slug>/<str:prod_slug>',views.productview,name='productview'),
     path('shop/',views.shop,name='shop'),
 
+    # authview.py page data link
     path('register/',authview.register, name='register'),
     path('login/',authview.loginpage, name='loginpage'),
     path('logout/',authview.logoutpage, name='logoutpage'),
 
+    # cart.py page data link
     path('add_to_cart',cart.addtocart, name='addtocart'),
     path('cart',cart.viewcart, name='viewcart'),
     path('update-cart',cart.updatecart, name='updatecart'),
     path('deletecartitem',cart.deletecartitem, name='deletecartitem'),
 
+    # wishlist.py page data link
     path('wishlist',wishlist.index, name='wishlist'),
     path('add_to_wishlist',wishlist.addtowishlist, name='addtowishlist'),
     path('deletewishlistitem',wishlist.deletewishlistitem, name='deletewishlistitem'),
     
+    # checkout.py page data link
     path('checkout',checkout.index, name='checkout'),
     path('placeorder',checkout.placeorder, name='placeorder'),
 
