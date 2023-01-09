44853f15cb62d6d45df824c8422abd13672d935f
aamirk1 <aamir.k7199@gmail.com>
2023-01-09 10:24:31 +0530

    update

--- triofit/main/models.py
+++ triofit/main/models.py
@@ -1,3 +1,95 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import datetime
+import os
+    
 # Create your models here.
+def get_file_path(request,filename):
+    orginal_filename = filename
+    nowTime = datetime.datetime.now().strftime('%y%m%d%H:%M%S')
+    filename = "%s%s" % (nowTime, orginal_filename)
+    return os.path.join('uploads/',filename)
+
+class Category(models.Model):
+    slug = models.CharField(("slug"), max_length=50,null=False,blank=False)
+    name = models.CharField(("Name"), max_length=150,null=False,blank=False)
+    image = models.ImageField(upload_to=get_file_path,null=False,blank=False)
+    description = models.TextField(("Description"),max_length=500,null=False,blank=False)
+    status = models.BooleanField(default=False,help_text="0=default, 1=Hidden")
+    trending = models.BooleanField(default=False,help_text="0=default, 1=trending")
+    meta_title = models.CharField(("Meta Title"), max_length=100,null=False,blank=False)
+    meta_keyword = models.CharField(("Meta Keyword"), max_length=250,null=False,blank=False)
+    meta_description = models.CharField(("Meta Description"), max_length=550,null=False,blank=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    slug = models.CharField(("slug"), max_length=50,null=False,blank=False)
+    name = models.CharField(("Name"), max_length=150,null=False,blank=False)
+    product_image = models.ImageField(upload_to=get_file_path,null=False,blank=False)
+    short_description = models.TextField(("Short Description"),max_length=200,null=False,blank=False)
+    quantity = models.IntegerField(null =False,blank=False)
+    description = models.TextField(("Description"),max_length=600,null=False,blank=False)
+    ingridient = models.TextField(("Ingridient"),default="Not Availabel",max_length=500,null=False,blank=False)
+    orginal_price = models.FloatField(null =False,blank=False)
+    selling_price = models.FloatField(null =False,blank=False)
+    status = models.BooleanField(default=False,help_text="0=default, 1=Hidden")
+    trending = models.BooleanField(default=False,help_text="0=default, 1=trending")
+    tag = models.CharField(max_length=150, null =False,blank=False)
+    meta_title = models.CharField(("Meta Title"), max_length=100,null=False,blank=False)
+    meta_keyword = models.CharField(("Meta Keyword"), max_length=250,null=False,blank=False)
+    meta_description = models.CharField(("Meta Description"), max_length=550,null=False,blank=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    product_qty = models.IntegerField(null=False,blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    fname = models.CharField(max_length=150,null=False)
+    lname = models.CharField(max_length=150,null=False)
+    email = models.CharField(max_length=50,null=False)
+    phone = models.CharField(max_length=50,null=False)
+    address = models.TextField(null=False)
+    city = models.CharField(max_length=150,null=False)
+    state = models.CharField(max_length=150,null=False)
+    country = models.CharField(max_length=150,null=False)
+    pincode = models.CharField(max_length=150,null=False)
+    total_price = models.FloatField(null=False)
+    payment_mode = models.CharField(max_length=150, null=False)
+    payment_id = models.CharField(max_length=250, null=True)
+    orderstatuses = (
+        ('Pending','Pending'),
+        ('Out For Shipping','Out For Shipping'),
+        ('Completed','Completed'),
+    )
+    status = models.CharField(max_length=150,choices=orderstatuses, default='Pending')
+    message = models.TextField(null=True)
+    tracking_no = models.CharField(max_length=150, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.tracking_no)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    price = models.FloatField(null=False)
+    quantity = models.IntegerField(null=False)
+    def __str__(self):
+        return '{} - {}'.format(self.order.id, self.order.tracking_no)
---
+++ triofit/main/static/main/js/custom.js
@@ -0,0 +1,135 @@
+$(document).ready(function() {
+    $('.increment-btn').click(function(e){
+        e.preventDefault();
+        var inc_value = $(this).closest('.product_data').find('.qty-input').val();
+        var value = parseInt(inc_value,10);
+        value = isNaN(value) ? 0: value;
+        if(value < 10){
+            value++;
+            $(this).closest('.product_data').find('.qty-input').val(value);
+        }
+    });
+
+    $('.decrement-btn').click(function(e){
+        e.preventDefault();
+        var dec_value = $(this).closest('.product_data').find('.qty-input').val();
+        var value = parseInt(dec_value,10);
+        value = isNaN(value) ? 0: value;
+        if(value>1){
+            value--;
+            $(this).closest('.product_data').find('.qty-input').val(value);
+        }
+    });
+    
+    $('.addToCart').click(function(e){
+        e.preventDefault();
+        var product_id = $(this).closest('.product_data').find(".prod_id").val();
+        var product_qty = $(this).closest('.product_data').find(".qty-input").val();
+        var token = $('input[name=csrfmiddlewaretoken]').val();
+        $.ajax({
+            method : "POST",
+            url: "/add_to_cart",
+            data: {
+                "product_id":product_id,
+                "product_qty":product_qty,
+                csrfmiddlewaretoken: token
+            },
+            success: function(response){
+                console.log(response);
+                alertify.success(response.status)
+                
+            }
+            
+        });
+
+    });
+
+
+    $('.changeQuantity').click(function(e){
+        e.preventDefault();
+        var product_id = $(this).closest('.product_data').find(".prod_id").val();
+        var product_qty = $(this).closest('.product_data').find(".qty-input").val();
+        var token = $('input[name=csrfmiddlewaretoken]').val();
+        $.ajax({
+            method : "POST",
+            url: "/update-cart",
+            data: {
+                "product_id":product_id,
+                "product_qty":product_qty,
+                csrfmiddlewaretoken: token
+            },
+            success: function(response){
+                // alertify.success(response.status)
+                $('.cartdata').load(location.href +" .cartdata");
+            }
+        });
+
+    });
+
+    $(document).on('click','.dalete-cart-item',function(e){
+        e.preventDefault();
+        var product_id = $(this).closest('.product_data').find(".prod_id").val();
+        var token = $('input[name=csrfmiddlewaretoken]').val();
+        $.ajax({
+            method : "POST",
+            url: "/deletecartitem",
+            data: {
+                "product_id":product_id,
+                csrfmiddlewaretoken: token
+            },
+            success: function(response){
+                console.log('delete')
+                $('.cartdata').load(location.href +" .cartdata");
+                alertify.success(response.status)    
+            }
+        });
+    });
+
+    $(document).on('click','.dalete-wishlist-item',function(e){
+        e.preventDefault();
+        var product_id = $(this).closest('.product_data').find(".prod_id").val();
+        var token = $('input[name=csrfmiddlewaretoken]').val();
+        $.ajax({
+            method : "POST",
+            url: "/deletewishlistitem",
+            data: {
+                "product_id":product_id,
+                csrfmiddlewaretoken: token
+            },
+            success: function(response){
+                console.log('delete')
+                $('.wishdata').load(location.href +" .wishdata");
+                alertify.success(response.status)    
+            }
+        });
+
+    });
+
+    $('.addToWishlist').click(function(e){
+        e.preventDefault();
+        var product_id = $(this).closest('.product_data').find(".prod_id").val();
+        var token = $('input[name=csrfmiddlewaretoken]').val();
+        $.ajax({
+            method : "POST",
+            url: "/add_to_wishlist",
+            data: {
+                "product_id":product_id,
+                csrfmiddlewaretoken: token
+            },
+            success: function(response){
+                console.log(response);
+                
+                alertify.success(response.status)
+                
+            }
+            
+        });
+
+    });
+    
+    
+
+});
+
+
+
